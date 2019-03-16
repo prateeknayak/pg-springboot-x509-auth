1c2b62928529a3a56314dfc9b2ea3cb9604f23ab
Prateek Nayak <6571322+prateeknayak@users.noreply.github.com>
2019-03-16 22:41:53 +1100

    Fixes the gencert script

--- bin/gencert.sh
+++ bin/gencert.sh
@@ -16,31 +16,6 @@ openssl req \
         -out ${certdir}/rootCA.crt \
         -subj "/C=ZZ/ST=BananaRepublic/L=ZombieLand/O=MonsterInc/OU=ScaresDivision/CN=My Root CA"
 
-
-openssl genrsa -out ${certdir}/msslserver.key 2048
-
-openssl req \
-        -new \
-        -sha256 \
-        -key ${certdir}/msslserver.key \
-        -reqexts SAN \
-        -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:*.svc.local")) \
-        -subj "/C=ZZ/ST=BananaRepublic/L=ZombieLand/O=MonsterInc/OU=DraculaDivison/CN=msslserver" \
-        -out ${certdir}/msslserver.csr
-
-
-openssl x509 \
-        -req \
-        -in ${certdir}/msslserver.csr \
-        -CA ${certdir}/rootCA.crt \
-        -CAkey ${certdir}/rootCA.key \
-        -CAcreateserial \
-        -out ${certdir}/msslserver.crt \
-        -days 500 \
-        -sha256
-
-openssl pkcs12 -export -password pass:password -in .pki/msslserver.crt -inkey .pki/msslserver.key -name msslserver -out .pki/msslserver.p12 -nodes
-
 ## Server Section
 openssl genrsa -out ${certdir}/msslserver.key 2048
 
@@ -49,7 +24,7 @@ openssl req \
         -sha256 \
         -key ${certdir}/msslserver.key \
         -reqexts SAN \
-        -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:*.svc.local")) \
+        -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:*.svc.local,DNS:localhost")) \
         -subj "/C=ZZ/ST=BananaRepublic/L=ZombieLand/O=MonsterInc/OU=DraculaDivison/CN=msslserver" \
         -out ${certdir}/msslserver.csr
 
@@ -65,3 +40,52 @@ openssl x509 \
         -sha256
 
 openssl pkcs12 -export -password pass:password -in .pki/msslserver.crt -inkey .pki/msslserver.key -name msslserver -out .pki/msslserver.p12 -nodes
+
+## App Section
+openssl genrsa -out ${certdir}/msslapp.key 2048
+
+openssl req \
+        -new \
+        -sha256 \
+        -key ${certdir}/msslapp.key \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:*.svc.local,DNS:localhost")) \
+        -subj "/C=ZZ/ST=BananaRepublic/L=ZombieLand/O=MonsterInc/OU=DraculaDivison/CN=msslapp" \
+        -out ${certdir}/msslapp.csr
+
+
+openssl x509 \
+        -req \
+        -in ${certdir}/msslapp.csr \
+        -CA ${certdir}/rootCA.crt \
+        -CAkey ${certdir}/rootCA.key \
+        -CAcreateserial \
+        -out ${certdir}/msslapp.crt \
+        -days 500 \
+        -sha256
+
+# openssl pkcs12 -export -password pass:password -in .pki/msslapp.crt -inkey .pki/msslapp.key -name msslapp -out .pki/msslapp.p12 -nodes
+
+
+openssl genrsa -out ${certdir}/msslapp2.key 2048
+
+openssl req \
+        -new \
+        -sha256 \
+        -key ${certdir}/msslapp2.key \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:*.svc.local")) \
+        -subj "/C=ZZ/ST=BananaRepublic/L=ZombieLand/O=MonsterInc/OU=DraculaDivison/CN=msslapp2" \
+        -out ${certdir}/msslapp2.csr
+
+
+openssl x509 \
+        -req \
+        -in ${certdir}/msslapp2.csr \
+        -CA ${certdir}/rootCA.crt \
+        -CAkey ${certdir}/rootCA.key \
+        -CAcreateserial \
+        -out ${certdir}/msslapp2.crt \
+        -days 500 \
+        -sha256
+
